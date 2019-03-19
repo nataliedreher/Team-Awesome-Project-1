10bc262348015f39443df5cc4dde4e15f9593248
PlatinumPi <goldpi101@gmail.com>
2019-03-19 13:19:55 -0600

    City search is working properly!

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,20 +1,24 @@
-var cityInput = "";
-
-event.preventDefault();
-cityInput = $('#exampleFormControlInput1').val().trim();
-
-var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + cityInput + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&radius=5000&key=";
+var cityInput;
 
 var corsResolve = "https://cors-anywhere.herokuapp.com/";
 
 var apiKey = "AIzaSyAF-eDyAVsjiIBHAVrCNFhwrHuHHGVtGqg";
 
-function initialize() {
-    $.ajax({
-        url: corsResolve + placesURL + apiKey,
-        method: 'GET',
-    }).then(function(response) {
-        console.log(response);
-    })
-}
-initialize();
+$('#find-city').on("click", function () {
+    event.preventDefault();
+
+    var temp = $('#exampleFormControlInput1').val().trim();
+    cityInput = temp.toString();
+    var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + cityInput + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&radius=5000&key=";
+
+
+    function initialize() {
+        $.ajax({
+            url: corsResolve + placesURL + apiKey,
+            method: 'GET',
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+    initialize();
+})
