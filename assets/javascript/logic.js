f03d1a350c206d9aa01ee8187cb213e94cf8114b
PlatinumPi <goldpi101@gmail.com>
2019-03-20 14:27:46 -0600

    Prototype: Console Log Fix

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -27,8 +27,6 @@ var cityInput;
 var fullName;
 
 var map;
-var lctn = "denver";
-var term = "italian";
 var category = "food"
 var API_key = "gRTZgWGEiqxJda4wW8d6vbqO0QQcPqP3wPx0JsOTiX58Vw2cTzMS9WCe3ihn_A9CO9nAY3q2G0aaYoFtaDzxG710uz1CwzcE8UKfhKz8B23h1ai94ArS6oRbTfeLXHYx";
 var myurl = 'https://cors-anywhere.herokuapp.com/' + "https://api.yelp.com/v3/businesses/search?" + "&location=" + lctn + "&openNow=true&categories=" + category + "&term=" + term;
@@ -40,7 +38,6 @@ $.ajax({
     },
     method: 'GET',
     success: function (data) {
-        console.log('success: ', data);
     }
 });
 
@@ -53,48 +50,10 @@ var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/js
 
 var corsResolve = "https://cors-anywhere.herokuapp.com/";
 
-    
+
 var apiKey = "AIzaSyAF-eDyAVsjiIBHAVrCNFhwrHuHHGVtGqg";
-var yelpName = [];
-var yelpCoordinates = [];
-var map;
-var category = "food"
-var API_key = "gRTZgWGEiqxJda4wW8d6vbqO0QQcPqP3wPx0JsOTiX58Vw2cTzMS9WCe3ihn_A9CO9nAY3q2G0aaYoFtaDzxG710uz1CwzcE8UKfhKz8B23h1ai94ArS6oRbTfeLXHYx";
-var myurl = 'https://cors-anywhere.herokuapp.com/' + "https://api.yelp.com/v3/businesses/search?" + "&location=" + lctn + "&openNow=true&sortBy=rating&limit=5&categories=" + category + "&term=" + term;
 
-
-//YELP API
-
-$.ajax({
-    url: myurl,
-    headers: {
-        'Authorization': 'Bearer ' + API_key,
-    },
-    method: 'GET',
-    success: function (data) {
-
-for (var i=0; i < data.businesses.length; i++) {
-    // console.log("Name: " + data.businesses[i].name);
-    yelpName.push(data.businesses[i].name);
-    console.log(yelpName);
-    var obj = {latitude: data.businesses[i].coordinates.latitude, longitude: data.businesses[i].coordinates.longitude};
-    yelpCoordinates.push(obj);
-    console.log(yelpCoordinates);
-    // console.log("Latitude: " + );
-    // console.log("Longitude: " + );
-
-}
-        console.log('success: ', data);
-    }
-});
-
-
-//GOOGLE PLACES API
-cityInput = $('#exampleFormControlInput1').val().trim();
-
-var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + cityInput + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&radius=5000&key=";
-
-$('#find-city').on("click", function (event) {
+$('#find-city').on("click", function () {
     event.preventDefault();
 
     var temp = $('#exampleFormControlInput1').val().trim();
@@ -107,8 +66,6 @@ $('#find-city').on("click", function (event) {
             url: corsResolve + placesURL + apiKey,
             method: 'GET',
         }).then(function (response) {
-            console.log(response);
-            console.log(response.candidates[0].formatted_address);
             latCity = (response.candidates[0].geometry.location.lat);
             lngCity = (response.candidates[0].geometry.location.lng);
             initMap(latCity, lngCity);
@@ -141,22 +98,15 @@ $(".logo-col").on('click', function () {
                 yelp.push(obj);
             }
 
-            console.log(yelp);
-            console.log('success: ', data);
-            console.log(yelp[0].name);
             var latLogo = yelp[0].latitude;
-            console.log(latLogo);
             var lngLogo = yelp[0].longitude;
-            console.log(lngLogo);
 
             for (var j = 0; j < yelp.length; j++) {
                 latLogo = yelp[j].latitude;
                 lngLogo = yelp[j].longitude;
                 var pushLatLng = { lat: latLogo, lng: lngLogo };
-                console.log(pushLatLng);
                 locations.push(pushLatLng);
             }
-            console.log(locations);
             initMap(latCity, lngCity);
         }
     });
