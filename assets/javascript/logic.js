2ff9479e7e279651a83f2df872ec2a69c4451262
PlatinumPi <goldpi101@gmail.com>
2019-03-20 13:52:35 -0600

    Prototype Complete Fixed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -46,7 +46,6 @@ $.ajax({
 
 var cityInput;
 
-event.preventDefault();
 cityInput = $('#exampleFormControlInput1').val().trim();
 
 var placesURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + cityInput + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&radius=5000&key=";
